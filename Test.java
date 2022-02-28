8a713ebc55cd6409db32708819c620517c3acdd6
saket <saketsingh8120@gmail.com>
2022-02-28 23:19:41 +0530

    change done

---
+++ Test.java
@@ -0,0 +1,17 @@
+class Test{
+   public static void main(String args[]){
+int lable =1;
+switch(label){
+case1: System.out.println("1st block");
+break;
+case1:  System.out.println("1st block");
+break;
+case1:  System.out.println("1st block");
+break;
+default:System.out.println("wrong choice");
+break;
+}
+System.out.println("finish");
+
+}
+}
